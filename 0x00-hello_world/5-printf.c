d52085b3b539ee53f22af930a9326cd5faa71fb5
victor <falolavictor426@gmail.com>
2022-09-08 15:49:42 -0700

    victor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,7 @@
-#include <stdio.h>
-/**
-* main - entry point
-* printf function
-* Return: 0
-*/    
-int main(void)
-{
+#include<stdio.h>
+int main(void){
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
+
+
 }
