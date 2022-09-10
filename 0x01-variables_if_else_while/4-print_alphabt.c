bd66518b8edbe3f206cff43db46a178b014d8597
victor <falolavictor426@gmail.com>
2022-09-10 01:54:31 +0000

    victor

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets in lowercase without e or q then followed by a new line
+ * using putchar
+ * Return; Always (Success)
+ */
+
+int main(void)
+{
+	int lw = 'a';
+	while (lw <= 'z')
+	{
+		if (lw == 'e'|| lw == 'q')
+		{
+			lw += 1;
+		}
+		else
+		{
+			putchar(lw);
+			lw += 1;
+		}
+	}
+	putchar('\n');
+	return(0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints single digit numbers from 0 to 10
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return(0);
+}
