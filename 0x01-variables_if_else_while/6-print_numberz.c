2716ec0c3f0702455c27e88cc98f64fce122d928
Victor <falolavictor426@gmail.com>
2022-09-10 02:53:06 +0000

    victor

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single dit number
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int d;
+	for (d ='0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints lowercase in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char low;
+	for (low ='z'; low >='a'; low--)
+	{
+		putchar(low);
+	}
+	putchaar('\n');
+	return (0);
