900fac28bf40d9ca39abb71608db7433952aafff
Victor <falolavictor426@gmail.com>
2022-09-13 23:20:54 +0000

    victor

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - check for lower case characters
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
