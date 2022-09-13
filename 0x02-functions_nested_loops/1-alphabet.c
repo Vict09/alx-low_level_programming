7a1534176af246cc827f7542ed8605dc67484318
Victor <falolavictor426@gmail.com>
2022-09-13 21:15:06 +0000

    victor

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ *main - prints the alphabets
+ *contains a function that print the alphabets
+ * Return: 0
+ */
+
+int alpha(void);
+int main(void)
+{
+	alpha();
+	_putchar('\n');
+	return
+}
+ int alpha(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+		return (0);
+	}
