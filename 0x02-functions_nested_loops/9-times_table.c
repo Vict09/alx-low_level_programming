8375fc9100a20a83ef6334c812971df2bd5bb6f2
Victor <falolavictor426@gmail.com>
2022-09-14 09:31:59 +0000

    victor

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,13 +13,13 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			res = (i * j);
-																		if (j != 0)
-																		{
+			if (j != 0)
+			{
 				_putchar(',');
 				_putchar(' ');
 																		}
 																		if (res >= 10)
-																		{
+			{
 																			_putchar((res / 10) + '0');
 																			_putchar((res % 10) + '0');
 																		}
