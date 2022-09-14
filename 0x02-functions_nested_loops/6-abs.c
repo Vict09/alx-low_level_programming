0224df2ecafc6052381b3caed0f86f8c2a7446dd
Victor <falolavictor426@gmail.com>
2022-09-14 01:57:50 +0000

    victor

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value 
+ * @c: is the integer that will be used in the function
+ * Return: 
+ */
+int -abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
