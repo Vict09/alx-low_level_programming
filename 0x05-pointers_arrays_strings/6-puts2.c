f41e582403c2c486cae3071c8ee7cdb3e11b833b
Victor <falolavictor426@gmail.com>
2022-09-19 22:20:18 +0000

    vict

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - print other character
+ * @str: the string
+ * Return (0)
+ */
+void puts2(char *str)
+{
+	int i;
+	int len;
+	len = _strlen(str);
+	for (i = 0; i < len + 1; i++)
+	{
+		if ( i % 2 != 0)
+		{
+			_putchar(str);
+		}
+	}
+}
