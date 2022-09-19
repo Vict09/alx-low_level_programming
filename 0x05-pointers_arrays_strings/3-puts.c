343f8506abb78aa16ecdd71070b59587a4df6820
Victor <falolavictor426@gmail.com>
2022-09-19 20:05:29 +0000

    victor

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string
+ * @str: the string
+ * Return 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str);
+	}
+}}
