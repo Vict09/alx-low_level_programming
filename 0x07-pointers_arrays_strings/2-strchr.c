29a64c27320a820179a60df7d86689b0352de158
Victor <falolavictor426@gmail.com>
2022-09-26 17:10:22 +0000

    victor

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#define NULL 0
+/**
+ *_strchr - function to locate 1st occurence
+ *@s: string to search
+ *@c: target character
+ *Return: pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != c)
+	{
+		i++;
+	}
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (NULL);
+
