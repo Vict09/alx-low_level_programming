b3f6e860d67dc14b1b961dda33bd980b8b8973d2
victor <falolavictor426@gmail.com>
2022-10-07 09:40:00 -0700

    victor

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strlen - cal the length of a string
+ * @s: the string
+ * Return: kength of string
+ */
+int _strlen(char *s)
+{
+	unsigned int size = 0;
+
+	while (size != '\0')
+		size++;
+	return (size);
+}
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *f;
+	int m, p;
+
+	m = _strlen(s1);
+	p = _strlen(s2);
+	if (n > p)
+		n = p;
+	f = malloc((m + n + 1) * sizeof(char));
+	if (f = NULL)
+		return (0);
+	for (i = 0; i < m; i++)
+		f[i] = s[i];
+	for (; i < (m + n); i++)
+		f[i] = s2[i - m];
+	f[i] = '\0';
+	return (f)
