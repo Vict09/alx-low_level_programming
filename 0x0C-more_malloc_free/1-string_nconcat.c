72e8b8ca381f0c0bdd08a135a7f50d541f623f19
victor <falolavictor426@gmail.com>
2022-10-07 09:51:47 -0700

    victor

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,6 +14,13 @@ int _strlen(char *s)
 }
 
 
+/**
+ * string_nconcat - concatenate strings
+ * @s1:first string
+ * @s2:second string
+ * @n:byte to concatenate
+ * Return:string containing n byte of s2
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *f;
@@ -24,11 +31,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > p)
 		n = p;
 	f = malloc((m + n + 1) * sizeof(char));
-	if (f = NULL)
+	if (f == NULL)
 		return (0);
 	for (i = 0; i < m; i++)
 		f[i] = s[i];
 	for (; i < (m + n); i++)
 		f[i] = s2[i - m];
 	f[i] = '\0';
-	return (f)
+	return (f);
